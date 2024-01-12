a77371b509769aeb1b39d690229242fe7c3e08b2
viowl <sovaworkbox@gmail.com>
2024-01-12 20:01:58 +0100

    timer finish

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,5 +1,91 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import iziToast from "izitoast/dist/js/iziToast.min.js";
+import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const datetimePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
+
+let countdownInterval;
+
+datetimePicker.flatpickr({
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date,
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const now = new Date();
+
+    if (selectedDate <= now) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topCenter'
+      });
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+});
+
+startButton.disabled = true;
+
+function updateTimerDisplay(time) {
+  daysValue.textContent = addLeadingZero(time.days);
+  hoursValue.textContent = addLeadingZero(time.hours);
+  minutesValue.textContent = addLeadingZero(time.minutes);
+  secondsValue.textContent = addLeadingZero(time.seconds);
+}
+
+function startCountdown(targetDate) {
+  clearInterval(countdownInterval);
+  datetimePicker.disabled = true
+
+  countdownInterval = setInterval(() => {
+    const now = new Date();
+    const difference = targetDate - now;
+
+    if (difference <= 0) {
+      clearInterval(countdownInterval);
+      updateTimerDisplay(convertMs(0));
+      iziToast.success({
+        title: 'Success',
+        message: 'Countdown finished!',
+        position: 'topRight'
+      });
+      startButton.disabled = false;
+    } else {
+      updateTimerDisplay(convertMs(difference));
+    }
+  }, 1000);
+}
+
+startButton.addEventListener('click', () => {
+  const selectedDate = new Date(datetimePicker._flatpickr.selectedDates[0]);
+  startButton.disabled = true;
+  startCountdown(selectedDate);
+});
+
+function addLeadingZero(value) {
+  return value < 10 ? `0${value}` : value;
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,27 @@
+
+const NOTIFICATION_DELAY = 3000;
+let timeoutId = null;
+
+const notification = document.querySelector(".js-alert");
+const showButton = document.querySelector(".js-show-alert");
+
+showButton.addEventListener("click", showNotification);
+notification.addEventListener("click", onNotificationClick);
+
+/*
+ * Функції
+ */
+function onNotificationClick() {
+  if (timeoutId) clearTimeout(timeoutId);
+  hideNotification();
+}
+
+function showNotification() {
+  notification.classList.add("is-visible");
+
+  timeoutId = setTimeout(hideNotification, NOTIFICATION_DELAY);
+}
+
+function hideNotification() {
+  notification.classList.remove("is-visible");
+}

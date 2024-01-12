38744d483da8635db7bd18ed2f0aa12aba06794f
viowl <sovaworkbox@gmail.com>
2024-01-12 12:47:18 +0100

    Dependencies

---
+++ src/js/1-timer.js
@@ -0,0 +1,5 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast/dist/js/iziToast.min.js";
+import "izitoast/dist/css/iziToast.min.css";
+
